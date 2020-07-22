7e54c4253b955b6f53571fbb61f53b47b6eb8365
Caio Augusto <caioag42@gmail.com>
2020-07-21 23:17:18 -0300

    back routes and seeds

---
+++ back/src/routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import knex from "./db/connection";
+
+const routes = express.Router();
+
+routes.get("/items", async (req, res) => {
+    const items = await knex("items").select("*");
+
+    return res.json(items)
+})
+
+export default routes;
