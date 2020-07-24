1fce1266e3443f33c44e98323e9b804a8a273480
Caio Augusto <caioag42@gmail.com>
2020-07-23 23:52:05 -0300

    show controller

--- back/src/controllers/PointsController.ts
+++ back/src/controllers/PointsController.ts
@@ -2,6 +2,22 @@ import knex from "../db/connection";
 import { Request, Response } from "express";
 
 class PointsController {
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex("points").where("id", id).first();
+
+    if (!point) {
+      return res.status(400).json({ message: "point not found" });
+    }
+
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id).select("items.title")
+
+    return res.json({ point, items });
+  }
+
   async create(req: Request, res: Response) {
     const {
       name,
--- back/src/routes.ts
+++ back/src/routes.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import knex from "./db/connection";
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 
@@ -8,7 +7,7 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get("/items", itemsController.index);
-
 routes.post("/points", pointsController.create);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
